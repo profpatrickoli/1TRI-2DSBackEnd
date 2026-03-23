7658a646ff4a8c56b13aae1a96b7122da06c0e13
Patrick <patrick.staroin@escola.pr.gov.br>
2026-03-23 07:38:04 -0300

    Aulas 5

---
+++ aula5.js
@@ -0,0 +1,16 @@
+// LISTAS (arrays)
+const aluno1 = "Amanda"
+const aluno2 = "Ariel"
+const aluno3 = "Carlos"
+
+// índice:         0        1        2           3
+const alunos = ["Amanda","Ariel", "Carlos", "Cristhyan"]
+const altura = [1.51    ,  1.80 ,   1.74  ,     1.53   ]
+
+let contador = 0 // variável de início
+while(contador <= 3){ // condição de parada
+    console.log(alunos[contador] + " - " + altura[contador])
+    contador = contador + 1 // incremento do laço
+}
+
+// criar um laço de repetição para contar de 0 até 1000000
